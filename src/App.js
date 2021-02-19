4a812c112d73a7da4a669b68d4ae8ce04b93c790
Igor Kudlo <igoreklida@gmail.com>
2021-02-19 10:57:18 +0300

    feat: useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div>
+    const [counter, setCounter] = useState(0);
 
-    </div>
-  );
+    function increment() {
+        setCounter((prevCounter) => {
+            return prevCounter + 1;
+        });
+    }
+
+    function decrement() {
+        setCounter(counter - 1);
+    }
+
+    return (
+        <div>
+            <h1>Счетчик: {counter}</h1>
+            <button onClick={increment} className="btn btn-success">Добавить</button>
+            <button onClick={decrement} className="btn btn-danger">Убрать</button>
+        </div>
+    );
 }
 
 export default App;
