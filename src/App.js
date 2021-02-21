ffd8d506cba78bd19d76ef91420c0a576b8d3b0a
IgorKudlo <igoreklida@gmail.com>
2021-02-21 15:38:31 +0300

    feat: useEffect example deps

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from "react";
 
 function App() {
     const [type, setType] = useState('users');
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-      console.log('render')
+      fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(response => response.json())
+        .then(json => setData(json))
     }, [type])
 
     return (
@@ -12,8 +15,10 @@ function App() {
             <h1>Ресурс: {type}</h1>
 
             <button onClick={() => setType('users')}>Пользователи</button>
-            <button onClick={() => setType('todo')}>Todos</button>
+            <button onClick={() => setType('todos')}>Todos</button>
             <button onClick={() => setType('posts')}>Посты</button>
+
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     );
 }
