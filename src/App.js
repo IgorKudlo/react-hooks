3771888d73a9ade3b13b03c23f8825ff93ca2314
IgorKudlo <igoreklida@gmail.com>
2021-02-21 15:30:04 +0300

    feat: useEffect one param

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function App() {
+    const [type, setType] = useState('users');
+
+    useEffect(() => {
+      console.log('render')
+    })
+
     return (
         <div>
+            <h1>Ресурс: {type}</h1>
 
+            <button onClick={() => setType('users')}>Пользователи</button>
+            <button onClick={() => setType('todo')}>Todo</button>
+            <button onClick={() => setType('posts')}>Посты</button>
         </div>
     );
 }
