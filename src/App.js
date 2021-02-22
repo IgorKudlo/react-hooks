c96367433c097742559ea3bb9dd1c27df2264337
Igor Kudlo <igoreklida@gmail.com>
2021-02-22 11:04:48 +0300

    refactor: update structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState} from "react";
 
 function App() {
-    const [value, setValue] = useState('initial');
-    const renderCount = useRef(1);
-    const inputRef = useRef(null);
-    const prevValue = useRef('');
-
-    useEffect(() => {
-        renderCount.current++;
-        console.log(inputRef.current.value)
-    });
-
-    useEffect(() => {
-        prevValue.current = value
-    }, [value]);
-
-    const focus = () => inputRef.current.focus();
+    const [number, setNumber] = useState(42);
 
     return (
-        <div>
-            <h1>Количество рендеров: {renderCount.current}</h1>
-            <h1>Прошлое состояние: {prevValue.current}</h1>
-            <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
-            <button className="btn btn-success" onClick={focus}>Фокус</button>
-        </div>
+        <>
+            <h1>Вычисляемое свойство: {number}</h1>
+            <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
+            <button className="btn btn-danger" onClick={() => setNumber(prev => prev + 1)}>Убрать</button>
+        </>
     );
 }
 
