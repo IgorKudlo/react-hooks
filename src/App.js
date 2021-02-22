3f1036c2fe20b750130a7815ec46373c4c6d6db1
Igor Kudlo <igoreklida@gmail.com>
2021-02-22 14:42:15 +0300

    refactor: update structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React, {useState, useMemo, useEffect} from "react";
-
-function complexComputed(num) {
-    console.log('complexComputed')
-    let i = 0;
-    while (i < 1000000000) i++
-    return num * 2;
-}
+import React, {useState} from "react";
 
 function App() {
-    const [number, setNumber] = useState(42);
     const [colored, setColored] = useState(false);
+    const [count, setCount] = useState(1);
 
-    const styles = useMemo(() => {
-        return {
-            color: colored ? 'darkred' : 'black'
-        }
-    }, [colored]);
-
-    const computed = useMemo(() => {
-        return complexComputed(number)
-    }, [number]);
-
-    useEffect(() => {
-        console.log('Styles changed')
-    }, [styles]);
+    const styles = {
+        color: colored ? 'darkred' : 'black'
+    }
 
     return (
         <>
-            <h1 style={styles} >Вычисляемое свойство: {computed}</h1>
-            <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
-            <button className="btn btn-danger" onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
+            <h1 style={styles} >Количество элеиентов: </h1>
+            <button className="btn btn-success" onClick={() => setPrev(prev => prev + 1)}>Добавить</button>
             <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Изменить</button>
         </>
     );
---
+++ src/examples/memo.js
@@ -0,0 +1,38 @@
+import React, {useState, useMemo, useEffect} from "react";
+
+function complexComputed(num) {
+    console.log('complexComputed')
+    let i = 0;
+    while (i < 1000000000) i++
+    return num * 2;
+}
+
+function App() {
+    const [number, setNumber] = useState(42);
+    const [colored, setColored] = useState(false);
+
+    const styles = useMemo(() => {
+        return {
+            color: colored ? 'darkred' : 'black'
+        }
+    }, [colored]);
+
+    const computed = useMemo(() => {
+        return complexComputed(number)
+    }, [number]);
+
+    useEffect(() => {
+        console.log('Styles changed')
+    }, [styles]);
+
+    return (
+        <>
+            <h1 style={styles} >Вычисляемое свойство: {computed}</h1>
+            <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
+            <button className="btn btn-danger" onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
+            <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Изменить</button>
+        </>
+    );
+}
+
+export default App;
