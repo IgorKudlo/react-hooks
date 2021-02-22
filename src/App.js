41a5040c03f2dae2683353f36e951a340cbcefe5
Igor Kudlo <igoreklida@gmail.com>
2021-02-22 10:51:39 +0300

    feat: useRef for native DOM-element

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import React, {useState, useEffect, useRef} from "react";
 function App() {
     const [value, setValue] = useState('initial');
     const renderCount = useRef(1);
+    const inputRef = useRef(null);
 
     useEffect(() => {
-        renderCount.current++
+        renderCount.current++;
+        console.log(inputRef.current.value)
     })
+
+    const focus = () => inputRef.current.focus();
 
     return (
         <div>
             <h1>Количество рендеров: {renderCount.current}</h1>
-            <input type="text" onChange={e => setValue(e.target.value)} value={value} />
+            <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
+            <button className="btn btn-success" onClick={focus}>Фокус</button>
         </div>
     );
 }
