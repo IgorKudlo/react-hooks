5b9bbdfba891dd6643200076c2daef1d7b36b3df
Igor Kudlo <igoreklida@gmail.com>
2021-02-22 10:44:09 +0300

    feat: useRef - return object - save state after render component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function App() {
+    const [value, setValue] = useState('initial');
+    const renderCount = useRef(1);
+
+    useEffect(() => {
+        renderCount.current++
+    })
 
     return (
         <div>
-
+            <h1>Количество рендеров: {renderCount.current}</h1>
+            <input type="text" onChange={e => setValue(e.target.value)} value={value} />
         </div>
     );
 }
